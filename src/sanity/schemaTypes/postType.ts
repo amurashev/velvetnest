3dc6a810acc0a628edafde8c3a768d1b6b3eecb8
Andrey Murashev <andreymurashev@gmail.com>
2025-02-20 14:52:49 +0700

    Add tags type

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -1,41 +1,41 @@
-import {DocumentTextIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { DocumentTextIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
 export const postType = defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
   icon: DocumentTextIcon,
   fields: [
     defineField({
-      name: 'title',
-      type: 'string',
+      name: "title",
+      type: "string",
     }),
     defineField({
-      name: 'slug',
-      type: 'slug',
+      name: "slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
       },
     }),
     defineField({
-      name: 'author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
     }),
     defineField({
-      name: 'mainImage',
-      type: 'image',
+      name: "mainImage",
+      type: "image",
       options: {
         hotspot: true,
       },
       fields: [
         defineField({
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative text',
-        })
-      ]
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        }),
+      ],
     }),
     // defineField({
     //   name: 'category',
@@ -43,28 +43,33 @@ export const postType = defineType({
     //   of: [defineArrayMember({type: 'reference', to: {type: 'category'}})],
     // }),
     defineField({
-      name: 'category',
-      type: 'reference',
-      to: { type: 'category' },
+      name: "category",
+      type: "reference",
+      to: { type: "category" },
     }),
     defineField({
-      name: 'publishedAt',
-      type: 'datetime',
+      name: "tags",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "tag" } })],
     }),
     defineField({
-      name: 'body',
-      type: 'blockContent',
+      name: "publishedAt",
+      type: "datetime",
+    }),
+    defineField({
+      name: "body",
+      type: "blockContent",
     }),
   ],
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
     },
   },
-})
+});
